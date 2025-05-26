56bfb7ced48b7d318fafcb9e02b59b09d30a8a64
ysh020702 <ysh020702@gmail.com>
2025-05-27 03:11:21 +0900

    업로드 적용

--- app/src/main/java/com/haedal/interviewhelper/data/repository/AudioRepositoryImpl.kt
+++ app/src/main/java/com/haedal/interviewhelper/data/repository/AudioRepositoryImpl.kt
@@ -4,18 +4,21 @@ import com.haedal.interviewhelper.data.remote.ApiService
 import com.haedal.interviewhelper.domain.repository.AudioRepository
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.asRequestBody
 import okhttp3.ResponseBody
 import retrofit2.Response
 import java.io.File
 import javax.inject.Inject
 
-class AudioRepositoryImpl @Inject constructor(
-    private val apiService: ApiService
-): AudioRepository{
+class AudioRepositoryImpl(
+    private val api: ApiService
+) : AudioRepository {
+
     override suspend fun uploadWav(file: File): Response<ResponseBody> {
-        val requestFile = file.asRequestBody("audio/wav".toMediaTypeOrNull())
+        val requestFile = file.asRequestBody("audio/*".toMediaTypeOrNull())
         val body = MultipartBody.Part.createFormData("file", file.name, requestFile)
-        return apiService.uploadAudio(body)
+
+        return api.uploadAudio(body)
     }
 }
